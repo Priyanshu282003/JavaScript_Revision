11fe5a98b3902426e3a543e457ece34de00fbb8f
Priyanshu Dubey <128988712+Priyanshu282003@users.noreply.github.com>
2024-11-09 19:47:34 +0000

    This is about variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accoutnEmail = "priyan0093@gmail.com"
+var accountPass="12345675"
+accountcity="Gorakhpur"
+let accountstate
+
+accoutnEmail="priya@1123"
+accountPass="2334223"
+accountcity="Pune"
+
+console.table([accountId, accoutnEmail, accountPass, accountcity,  accountstate])
+/*
+prefer not to use var 
+because of issue of block scope and functional scope
+*/
