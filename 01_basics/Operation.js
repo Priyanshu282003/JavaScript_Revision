5c3abc4c6548e04b09a596727368747601641ffb
Priyanshu Dubey <128988712+Priyanshu282003@users.noreply.github.com>
2024-11-11 20:08:09 +0000

    Operation in javaScript

---
+++ 01_basics/Operation.js
@@ -0,0 +1,20 @@
+let value=3
+let check=-value
+console.log(check)
+
+let s1="Priya"
+let s2="Dubey"
+let s3= s1 + s2
+console.log(s3)
+
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+
+let num1, num2, num3;
+num1=num2=num3=2+2;
+console.log(num1)
+
+let count=100
+++count
+console.log(count)
