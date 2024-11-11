41f858215f19d4088eaf4e85bd43de55ac924848
Priyanshu Dubey <128988712+Priyanshu282003@users.noreply.github.com>
2024-11-11 19:35:45 +0000

    This is a datatype_conversion

---
+++ 01_basics/datatype_con.js
@@ -0,0 +1,21 @@
+let score="100abc"
+let varN=Number(score)
+console.log(typeof(varN))
+console.log(varN)
+
+let marks=null
+console.log(typeof marks)
+let mark=undefined
+console.log(typeof mark)
+
+let logedIn=1
+let boollogedIn=Boolean(logedIn)
+console.log(boollogedIn)
+
+let empty=""
+let check=Boolean(empty)
+console.log(check)
+
+let string=33
+let chcecks=String(string)
+console.log(typeof chcecks)
