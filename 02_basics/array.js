07473f71e3f098f9f6f8d2b1646379236ec1c6f7
Priyanshu Dubey <128988712+Priyanshu282003@users.noreply.github.com>
2024-11-13 21:25:22 +0000

    array config done

---
+++ 02_basics/array.js
@@ -0,0 +1,32 @@
+const arr=[1, 2, 3, 4, 8, 5]
+console.log(arr[0])
+
+//Array Method
+
+// arr.push(23)
+// console.log(arr)
+// arr.unshift(3)
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+
+console.log(arr.includes(7))
+console.log(arr.indexOf(10))
+console.log(arr.indexOf(8))
+
+const newArr=arr.join()// it convert the array into the string 
+console.log(arr)
+console.log(newArr)
+console.log(typeof(newArr))
+
+// difference between slice and splice 
+
+const newArray=arr.slice(1, 3)
+console.log(arr)
+console.log(newArray)
+
+console.log(arr)
+const newArray2=arr.splice(1, 3)
+console.log(newArray2)
