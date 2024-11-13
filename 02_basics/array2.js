ea9ad51134feeb149547199d44d92a70e160a073
Priyanshu Dubey <128988712+Priyanshu282003@users.noreply.github.com>
2024-11-13 21:42:40 +0000

    array2 cnfig

---
+++ 02_basics/array2.js
@@ -0,0 +1,6 @@
+const fruit=["apple", "banana", "mango", "cheery"]
+const vegitable=["patato", "tamato", "ladyfingure", "cabbage"]
+// fruit.push(vegitable)
+// console.log(fruit)
+ const veg=fruit.concat(vegitable)
+ console.log(veg)
