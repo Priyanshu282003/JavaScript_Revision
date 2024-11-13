9dfb4b6ad5c8c6576814c4e737a7c520e7f7aede
Priyanshu Dubey <128988712+Priyanshu282003@users.noreply.github.com>
2024-11-13 20:50:25 +0000

    date config done

---
+++ 01_basics/date.js
@@ -0,0 +1,15 @@
+let myDate=new Date()
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleDateString())
+
+let createdDate=new Date(12, 0, 23)
+console.log(createdDate.toDateString())
+
+let myTimestamp=Date.now()
+console.log(myTimestamp)
+
+let newDate= new Date()
+console.log(newDate)
+console.log(newDate.getDay().toString())
+console.log(newDate.getMonth())
+console.log(newDate.getMonth()+1)
