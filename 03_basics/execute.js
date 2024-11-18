e57068de64eed310c736bb655c250dec2d66cab8
Priyanshu Dubey <128988712+Priyanshu282003@users.noreply.github.com>
2024-11-18 19:45:21 +0000

    eexecute.config

---
+++ 03_basics/execute.js
@@ -0,0 +1,5 @@
+// JavaScript Execution Context 
+// {}-Global Execution context it is first alloacte inside "this"
+//Lets see how code are execute in javaScript
+//{}-First is memory creation phase 
+// ->Second phase is execution phase
