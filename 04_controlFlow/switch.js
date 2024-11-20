12925446be8bef49d5aaa6b4dad143441d38317e
Priyanshu Dubey <128988712+Priyanshu282003@users.noreply.github.com>
2024-11-20 04:52:03 +0000

    switch.config

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,23 @@
+
+const month=3
+switch (month) {
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+
+    default:
+        console.log("default case execute");
+        
+        break;
+}
+
+//If break is not there is run all the cases except default
