b852df6259f473ea738b1eb14edd543fdf98559f
Priyanshu Dubey <128988712+Priyanshu282003@users.noreply.github.com>
2024-11-20 04:41:17 +0000

    one.config

--- 04_controlFlow/one.js
+++ 04_controlFlow/one.js
@@ -5,3 +5,26 @@ if(age==="20"){
 else{
     console.log("You can drink")
 }
+
+// let a =200
+// if(a>20){
+//     let power="fly"
+    
+// }
+// console.log(` The super power is ${power}`)
+
+//Short hand notation
+const balance=100000
+if(balance>5000)console.log("test");// Here we assume that there is a scope "{}".
+
+const test1=true
+const test2=true
+if(test1&&test2){
+    console.log("All test cases are passed")
+}
+
+const userLogin=true
+const googlelogin=true
+if(userLogin||googlelogin){
+    console.log("Allow to sign in ")
+}
