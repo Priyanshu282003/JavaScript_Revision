8512293675fd00c96469f8c55a299342a7856d67
Priyanshu Dubey <128988712+Priyanshu282003@users.noreply.github.com>
2024-11-20 09:12:11 +0000

    while.config

---
+++ 05_iterations/while.js
@@ -0,0 +1,15 @@
+
+let socurce=1
+while (socurce<20) {
+    console.log(`Value of index ${socurce}`);
+    socurce++;
+}
+
+
+
+let score=1
+do {
+    console.log(`Score is ${score}`);
+    score++;
+    
+} while (score<=10);
